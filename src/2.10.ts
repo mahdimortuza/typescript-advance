5a3ec039c8b54c8f9d8dc29d199e49c4bed16c06
mahdimortuza <mahdi.webx@gmail.com>
2023-11-08 18:20:25 +0600

    mapped type

---
+++ src/2.10.ts
@@ -0,0 +1,37 @@
+{
+    // mapped types 
+
+    const arrowOfNumbers: number[] = [1, 4, 5]
+    // const arrOfStrings: string[] = ['1', '4', '5']
+    const arrOfStrings: string[] = arrowOfNumbers.map((number) => number.toString()
+
+    )
+    console.log(arrOfStrings);
+
+    type AreaNumber = {
+        height: string
+        width: number
+    }
+
+    type Height = AreaNumber["height"] // look up type
+
+    // T => {height: string; width:number}
+    // key => ["height"]
+
+    type AreaString<T> = {
+        [key in keyof T]: T[key]
+    }
+
+    const area1: AreaString<{ height: string; width: number }> = {
+        height: "100"
+    }
+
+    console.log("object");
+
+
+
+
+
+
+
+}
