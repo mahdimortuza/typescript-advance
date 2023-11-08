e31dc5a6d080916657ff32d8aa1b1e70decc39f4
mahdimortuza <mahdi.webx@gmail.com>
2023-11-08 18:58:17 +0600

    record

--- dist/2.11.js
+++ dist/2.11.js
@@ -1,3 +1,15 @@
 "use strict";
 {
+    const person1 = {
+        name: "Mr. XY",
+        age: 200,
+        contactNo: "017"
+    };
+    const EmptyObj = {};
+    const obj1 = {
+        a: "aa",
+        b: "bb",
+        c: "cc",
+        d: "dd"
+    };
 }
--- src/2.11.ts
+++ src/2.11.ts
@@ -1,3 +1,53 @@
 {
-    
+    // utility types
+    // pick
+
+    type Person = {
+        name: string
+        age: number
+        email?: string
+        contactNo: string
+    }
+
+    type NameAge = Pick<Person, "name" | "age">
+
+
+    // omit
+    type ContactInfo = Omit<Person, "name" | "age">
+
+
+    //required
+    type PersonRequired = Required<Person>
+
+    // partial
+    type PersonPartial = Partial<Person>
+
+    // readonly
+    type PersonReadOnly = Readonly<Person>
+
+
+    const person1: PersonReadOnly = {
+        name: "Mr. XY",
+        age: 200,
+        contactNo: "017"
+    }
+
+    // person1.name = "Mr. YZ"
+
+    // record 
+    // type MyObj = {
+    //     a: string
+    //     b: string
+    // }
+
+    type MyObj = Record<string, string>
+
+    const EmptyObj: Record<string, unknown> = {}
+
+    const obj1: MyObj = {
+        a: "aa",
+        b: "bb",
+        c: "cc",
+        d: "dd"
+    }
 }
